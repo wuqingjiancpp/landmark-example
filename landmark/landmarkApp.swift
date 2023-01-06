b08582c22b912f93bf9faedca41eeee82c0f303c
wuqingjian <crazywu@126.com>
2023-01-06 19:25:40 +0800

    Initial Commit

---
+++ landmark/landmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarkApp.swift
+//  landmark
+//
+//  Created by 吴庆健 on 2023/1/6.
+//
+
+import SwiftUI
+
+@main
+struct landmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
